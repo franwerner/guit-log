94f4dbd2b60a4418c85de741a0bc1b1c624a0e71
Franco Werner <ifrank44445@gmail.com>
2026-03-20 21:22:11 +0000

    init

---
+++ src/main/java/com/guitlog/guitlog/GuitlogApplication.java
@@ -0,0 +1,13 @@
+package com.guitlog.guitlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuitlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuitlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guitlog/guitlog/GuitlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guitlog.guitlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuitlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
